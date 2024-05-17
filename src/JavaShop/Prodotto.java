5049303856cc5d1565733c7c725cdbd9b59a66b4
Amarildo <you@example.com>
2024-05-17 16:07:35 +0200

    Add utility methods to prodotto class

--- src/JavaShop/Prodotto.java
+++ src/JavaShop/Prodotto.java
@@ -1,4 +1,66 @@
 package JavaShop;
 
+import java.util.Random;
 public class Prodotto {
+
+    //Atributi privati della classe
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private double iva;
+
+    //costruttore nuovo progetto
+    public Prodotto(String name, String description, double price, double iva) {
+        //nuovo codice random per il prodotto
+        this.code = generateRandomCode();
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
+
+    private int generateRandomCode() {
+        Random rand = new Random();
+        return rand.nextInt(1000000);
+    }
+  //getter code proddotto
+    public String getCode() {
+        return String.format("%06d", code);
+    }
+
+    //getter e setter name ecc...
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Getter e setter per il campo "prezzo"
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getter e setter per il campo "iva"
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
 }
